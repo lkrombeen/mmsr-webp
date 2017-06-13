Fix metric calculation

helper.py
@@ -14,16 +14,18 @@ class File():
 		file.diff = self.size - file.size
 		image_original = cv2.imread("input/" + self.name)
 		image_compressed = cv2.imread(dir_output + file.name)
-
 		file.error = self.compute_error(image_original, image_compressed)
 		self.compressed.append(file)
+		return file
 
 	def compute_error(self, image0, image1):
 		number_of_channels = np.shape(image0)[2]
 		total_error = 0
 		for channel_index in range(number_of_channels):
 			error = np.abs(image0[:,:,channel_index] - image1[:,:,channel_index])
-			total_error = total_error + (np.average(error)/(np.max(error) - np.min(error)))
+			maxmin = np.max(error) - np.min(error)
+			if (maxmin != 0):
+				total_error += (np.average(error)/maxmin)
 
 		return (total_error/number_of_channels)
 

metric.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import os
 import numpy as np
 import cv2
@@ -6,22 +7,32 @@ from os.path import basename
 from helper import File, Compressed
 
 dir = "input/"
-metric = []
+header = "Original Image;JPEG-100 Size Diff;JPEG-100 Size Diff (%);JPEG-100 Error;JPEG-80 Size Diff;JPEG-80 Size Diff (%);JPEG-80 Error;JPEG-50 Size Diff;JPEG-50 Size Diff (%);JPEG-50 Error;PNG-100 Size Diff;PNG-100 Size Diff (%);PNG-100 Error;PNG-80 Size Diff;PNG-80 Size Diff (%);PNG-80 Error;PNG-50 Size Diff;PNG-50 Size Diff (%);PNG-50 Error;BMP-100 Size Diff;BMP-100 Size Diff (%);BMP-100 Error;BMP-80 Size Diff;BMP-80 Size Diff (%);BMP-80 Error;BMP-50 Size Diff;BMP-50 Size Diff (%);BMP-50 Error;WEBP Size Diff;WEBP Size Diff (%);WEBP Error\n"
+results = ""
+
 for file in os.listdir(dir):
 	if not file.startswith('.'):
 		size = os.stat(dir + file).st_size
 		original = File(file, size)
-		dir_output = "output/" + basename(file.split('.')[0]) + "/"
-		for output in os.listdir(dir_output):
-			size_output = os.stat(dir_output + output).st_size
-			original.addCompressed(Compressed(output, size_output))
+		file_nonformat = basename(file.split('.')[0])
+		dir_output = "output/" + file_nonformat + "/"
+		result = file
 
-		metric.append(original)
+		for format in ["jpg", "png", "bmp"]:
+			for quality in ["100", "80", "50"]:
+				output = file_nonformat + "-quality" + quality + "." + format
+				size_output = os.stat(dir_output + output).st_size
+				compressed = original.addCompressed(Compressed(output, size_output))
+				result += ";" + str(compressed.diff) + ";" + str(np.round((compressed.diff / size) * 100, decimals = 2)) + ";" + str(compressed.error)
 
-for file in metric:
-	print ("Original image: " + file.name)
-	for compressed in file.compressed:
-		print ("Compressed image: " + compressed.name)
-		print ("Size difference: " + str(compressed.diff))
-		print ("Lossless error: " + str(compressed.error))
-	print ("\n")
+		output = file_nonformat + ".webp"
+		size_output = os.stat(dir_output + output).st_size
+		compressed = original.addCompressed(Compressed(output, size_output))
+		result += ";" + str(compressed.diff) + ";" + str(np.round((compressed.diff / size) * 100, decimals = 2)) + ";" + str(compressed.error)
+
+		results += result + "\n"
+		with open(dir_output + "/results.csv", "w") as text_file:
+			text_file.write(header + result)
+
+with open("output/results.csv", "w") as text_file:
+	text_file.write(header + results)