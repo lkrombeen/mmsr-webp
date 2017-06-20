efceff7d8f26ad63ab9855b3e27515385025fa7a
Tim van der Lippe <tvanderlippe@gmail.com>
2017-06-20 11:52:12 +0200

    Update metric script to include lossy webp

--- metric.py
+++ metric.py
@@ -7,8 +7,18 @@ from os.path import basename
 from helper import File, Compressed
 
 dir = "input/"
-header = "Original Image;JPEG-100 Size Diff;JPEG-100 Size Diff (%);JPEG-100 Error;JPEG-80 Size Diff;JPEG-80 Size Diff (%);JPEG-80 Error;JPEG-50 Size Diff;JPEG-50 Size Diff (%);JPEG-50 Error;PNG-100 Size Diff;PNG-100 Size Diff (%);PNG-100 Error;PNG-80 Size Diff;PNG-80 Size Diff (%);PNG-80 Error;PNG-50 Size Diff;PNG-50 Size Diff (%);PNG-50 Error;BMP-100 Size Diff;BMP-100 Size Diff (%);BMP-100 Error;BMP-80 Size Diff;BMP-80 Size Diff (%);BMP-80 Error;BMP-50 Size Diff;BMP-50 Size Diff (%);BMP-50 Error;WEBP Size Diff;WEBP Size Diff (%);WEBP Error\n"
+header = "Original Image;"
 results = ""
+formats = ["jpg", "png", "bmp", "webp"]
+qualities = ["100", "80", "50"]
+
+for format in formats:
+	for quality in qualities:
+		header += format + "-" + quality + " Size Diff;"
+		header += format + "-" + quality + " Size Diff (%);"
+		header += format + "-" + quality + " Error;"
+
+header += "WEBP Size Diff;WEBP Size Diff (%);WEBP Error\n"
 
 for file in os.listdir(dir):
 	if not file.startswith('.'):
@@ -18,8 +28,8 @@ for file in os.listdir(dir):
 		dir_output = "output/" + file_nonformat + "/"
 		result = file
 
-		for format in ["jpg", "png", "bmp"]:
-			for quality in ["100", "80", "50"]:
+		for format in formats:
+			for quality in qualities:
 				output = file_nonformat + "-quality" + quality + "." + format
 				size_output = os.stat(dir_output + output).st_size
 				compressed = original.addCompressed(Compressed(output, size_output))
